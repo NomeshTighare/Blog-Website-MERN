d4555c75270681d7a9b7cc0ef60ee368ba679f90
NomeshTighare <nomeshtighare@gmail.com>
2023-08-23 00:51:39 +0530

    ch

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDatabase =async  () => {
-
+    mongoose.set('strictQuery', true);
     await mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
