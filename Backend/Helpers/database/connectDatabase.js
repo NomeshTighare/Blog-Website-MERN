ff1f98ae4355610e6ae2de5994ed737330e26b2b
NomeshTighare <nomeshtighare@gmail.com>
2023-08-23 00:55:07 +0530

    dd

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose")
 require("dotenv").config({ path: "./config/.env" });
 const connectDatabase =async  () => {
     mongoose.set('strictQuery', true);
-    await mongoose.connect(process.env.MONGO_URI,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    })
+    await mongoose.connect(process.env.MONGO_URI)
 
     console.log("MongoDB Connection Successfully")
 
