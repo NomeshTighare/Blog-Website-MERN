a14427b0e03143d15781aa5e02fb733415f1252d
NomeshTighare <nomeshtighare@gmail.com>
2023-08-23 00:54:43 +0530

    chah

--- Backend/Helpers/database/connectDatabase.js
+++ Backend/Helpers/database/connectDatabase.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+require("dotenv").config({ path: "./config/.env" });
 const connectDatabase =async  () => {
     mongoose.set('strictQuery', true);
     await mongoose.connect(process.env.MONGO_URI,{
